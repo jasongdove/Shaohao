16a53ad9360f458bb073ba511bae477174f7f9e7
SargeroDeV <neohd251535@gmail.com>
2024-07-10 10:04:55 -0400

    Removed scripts not compatible with MoP, ScriptName in db will be removed later.

--- src/server/scripts/Pet/pet_script_loader.cpp
+++ src/server/scripts/Pet/pet_script_loader.cpp
@@ -16,21 +16,11 @@
  */
 
 // This is where scripts' loading functions should be declared:
-void AddSC_deathknight_pet_scripts();
 void AddSC_generic_pet_scripts();
-void AddSC_hunter_pet_scripts();
-void AddSC_mage_pet_scripts();
-void AddSC_priest_pet_scripts();
-void AddSC_shaman_pet_scripts();
 
 // The name of this function should match:
 // void Add${NameOfDirectory}Scripts()
 void AddPetScripts()
 {
-    AddSC_deathknight_pet_scripts();
     AddSC_generic_pet_scripts();
-    AddSC_hunter_pet_scripts();
-    AddSC_mage_pet_scripts();
-    AddSC_priest_pet_scripts();
-    AddSC_shaman_pet_scripts();
 }
